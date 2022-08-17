c76d71450b3dfac89b193e41b8ae17702c07b4bd
Yahia Jad <yahia.jad@gmail.com>
2022-08-17 20:30:45 +0200

    home

--- pages/1- 📂 Summarize  Log File.py
+++ pages/1- 📂 Summarize  Log File.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plogs as logs 
 
-uploaded_files= st.file_uploader('Select Log file',type=["zip"], accept_multiple_files = False)
+uploaded_files= st.file_uploader('Select Log file',type=["zip", 'gz', 'csv'], accept_multiple_files = False)
 if st.button('Process ...') and uploaded_files:
     with st.spinner("Please Wait ... "):
         logs.summerize_portal_logs(uploaded_files, load_db=False)

d0db45b37a5dd565e539e6c729e492ed3f90a437
Yahia <Yahia.Jad@gmail.com>
2023-08-06 17:01:39 +0300

    sda

---
+++ pages/2- Portal Projects.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import plogs as logs 
+import DB as db
+import Home as stu
+import pandas as pd
+
+
+def portal_projects():
+    df = db.query_to_pd('project')
+    df = df.set_index('project_id', drop=True).sort_values('project_id', ascending=False).drop(columns=['index'])
+    # df.columns
+    df[df.columns[7:12]]  = df[df.columns[7:12]].fillna(0).astype(int)
+    df = df.drop(columns=['Unnamed: 3', 'Unnamed: 11', 'Unnamed: 13'], axis=1)
+    df = df\
+        .assign(pcnt_paid_to_apps = (df['No. of reservations']/df['No. of paid customers']*100).fillna(0).astype(int))\
+        .assign(pcnt_res_to_paid = (df['No. of paid customers']/df['No. of applications']*100).fillna(0).astype(int))\
+        .assign(pcnt_res_to_paid = (df['No. of paid customers']/df['No. of applications']*100).fillna(0).astype(int))\
+        .assign(paid_to_units_lands = (df['No. of paid customers']/df['No. of units/lands']*100).fillna(0).astype(int))
+    # df.columns
+    # df
+    col_list = [0, 1, 6, 7,8,9,10,11,12]
+    stats = df[df.columns[col_list]]#.set_index('project_id')
+    
+
+    # st.dataframe(stats.groupby('project_type_name_ar').count().columns)
+    # st.dataframe(stats.groupby('project_type_name_ar').count()['project_name_ar'])
+
+    stats_2 = stats.drop(columns=['project_name_ar', 'pcnt_paid_to_apps', 'pcnt_paid_to_apps', 'pcnt_res_to_paid' ])\
+    .groupby('project_type_name_ar').sum().astype(int)\
+    .assign(no_projects = df.groupby('project_type_name_ar').count()['project_name_ar'])\
+    
+    stats_2.loc['الإجمالى'] = stats_2.sum() 
+
+    stats_2 = stats_2\
+    .assign(pcnt_paid_to_apps = lambda x:(x['No. of reservations']/x['No. of paid customers']*100).fillna(0).astype(int))\
+    .assign(pcnt_res_to_paid  = lambda x: (x['No. of paid customers']/x['No. of applications']*100).fillna(0).astype(int))\
+    .assign(paid_to_units_lands = lambda x: (x['No. of paid customers']/x['No. of units/lands']*100).fillna(0).astype(int))
+
+    stats_2 = stats_2.rename({
+        'no_projects':'عدد المشاريع',
+        'No. of units/lands': 'عدد الاراضى/الوحدات المطروحة',
+        'No. of applications': 'إجمالى عدد الإستمارات',
+        'No. of paid customers': 'عدد من سدد جدية الحجز',
+        'No. of reservations': 'عدد الحجوزات',
+        'paid_to_units_lands': 'نسبة جدية الحجز الى الوحدات المطروحة',
+        'pcnt_paid_to_apps': '% جدية الحجز الى الاستمارات',
+        'pcnt_res_to_paid': '% الحجز الى من سدد',
+    }, axis=1)
+
+    stats_2 = stats_2[stats_2.columns[[5,0,1,2,3,4,6,7]]].T     # reorder and transpose
+    # st.dataframe(stats_2.T)
+
+    st.subheader("Reservation Portal Dashboard")
+    st.dataframe(stats_2)
+
+    st.subheader("Reservation Portal Dashboard-Details")
+    stats = stats.rename({
+        'no_projects':'عدد المشاريع',
+        'No. of units/lands': 'عدد الاراضى/الوحدات المطروحة',
+        'No. of applications': 'إجمالى عدد الإستمارات',
+        'No. of paid customers': 'عدد من سدد جدية الحجز',
+        'No. of reservations': 'عدد الحجوزات',
+        'paid_to_units_lands': 'نسبة جدية الحجز الى الوحدات المطروحة',
+        'pcnt_paid_to_apps': '% جدية الحجز الى الاستمارات',
+        'pcnt_res_to_paid': '% الحجز الى من سدد',
+    }, axis=1)
+    st.dataframe(stats)
+
+def protal_stats_1():
+    pass
+
+options={'...':None, 
+            '1- Portal Projects Dashboard': portal_projects,
+            '2- Portal Projects Stats-1':protal_stats_1,
+            # '3- Units- Mismatched Checksum': None,
+        }
+
+opt = st.sidebar.selectbox("Options",options.keys())
+if options[opt]: 
+    options[opt]() # execute option
